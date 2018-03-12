6fe7d0a6ac4a7bdf4e5eba21746956fc2e17fdc1
Brad Searle <bradley.c.searle@accenture.com>
2018-03-12 13:46:03 -0500

    Add tab overflow functionality, begin styling of post title/user section

--- snootube.js
+++ snootube.js
@@ -21,6 +21,7 @@ class SnooTubeMaterial {
     let observer = new MutationObserver((mutations) => {
       if( document.getElementById('comments') ) {
         let ytComments = document.getElementById('comments');
+        window.ytComments = ytComments;
         let newComments = document.createElement('div');
         newComments.id = 'comments';
         newComments.className = 'ytd-watch snootube-container';
@@ -76,6 +77,28 @@ class SnooTubeMaterial {
 
   // }
 
+  appendTabs( tabList, tabContainer ) {
+    // Sort posts by number of upvotes
+    tabList.sort((a,b) => parseInt(b.getAttribute('score')) - parseInt(a.getAttribute('score')));
+
+    // Filter duplicate posts in one subreddit
+    let subs = [];
+    for( let i=0; i<tabList.length; i++ ) {
+      let el = tabList[i];
+      if( !subs.includes(el.dataset.sub) ) {
+        subs.push(el.dataset.sub);
+      } else {
+        tabList.splice(i--, 1);
+      }
+    }
+
+    // If number of threads greater than 4, add dropdown element with subchildren
+    if( tabList.length > 4 ) {
+      tabList.push( ContentRenderer.generateTabDropdown(tabList.splice(4)) );
+    }
+    tabList.forEach((el) => tabContainer.appendChild(el));
+  }
+
   async showThreadResults( threadList ) {
     let tabContainer = document.createElement('div'),
         threadContainer = document.createElement('div');
@@ -87,22 +110,28 @@ class SnooTubeMaterial {
     let tabList = [];
     await Promise.all(threadList.map(async (item) => {
       if( item.data.url.match(YoutubeData.getVideoId()) ) {
+        item.data.score = item.data.score.toLocaleString();
         let tab = await ContentRenderer.renderTab( item.data );
         tab.addEventListener('click', (e) => {
           console.log(e.target.dataset.id);
           document.querySelectorAll('.snootube-post-body').forEach((el) => el.classList.remove('visible'));
+          document.querySelectorAll('.snootube-tab').forEach((el) => el.classList.remove('active'));
           document.getElementById(e.target.dataset.id).classList.add('visible');
+          e.target.classList.add('active');
         });
         tabList.push(tab);
 
+        item.data.post_time = ContentRenderer.getDateStringFromTimestamp(item.data.created_utc);
         let thread = await ContentRenderer.renderPost( item.data );
         threadContainer.appendChild(thread);
       }
     }));
-    tabList.sort((a,b) => parseInt(b.getAttribute('score')) - parseInt(a.getAttribute('score')));
-    tabList.forEach((el) => tabContainer.appendChild(el));
+
+    this.appendTabs(tabList, tabContainer);
+
 
     this.hideLoadingScreen();
+    document.querySelector('.snootube-tab').click();
     tabContainer.classList.add('visible');
     threadContainer.classList.add('visible');
   }
@@ -136,8 +165,82 @@ class ContentRenderer {
     return this._templatize(ContentRenderer.postTemplate, redditResult);
   }
 
-  static _getTimeAgo( postTime ) {
-    let now = Date.now();
+  static generateTabDropdown( elements ) {
+    let dropdownContainer = document.createElement('div'),
+        dropdownList = document.createElement('ul');
+
+    // Setup for dropdown container
+    dropdownContainer.className = 'snootube-tab snoo-dropdown';
+
+    // Setup for dropdown list
+    dropdownList.className = 'snoo-dropdown-container';
+
+    dropdownList.addEventListener('click', function(e) {
+      // Delegate event to swap selected dropdown elements into the top bar
+      if(e.target && e.target.nodeName === 'BUTTON' && e.target.classList.contains('snootube-tab')) {
+        let toSwap = document.querySelector('.snoo-tabs > .snootube-tab:not(.snoo-dropdown):last-of-type'),
+            newLocation = e.target.parentElement;
+        e.target.remove();
+        toSwap.replaceWith(e.target);
+        newLocation.appendChild(toSwap);
+      }
+    });
+
+    for( let i=0; i<elements.length; i++ ) {
+      let listItem = document.createElement('li');
+      listItem.appendChild(elements[i]);
+      dropdownList.appendChild(listItem);
+    }
+
+    // Append elements together and return
+    dropdownContainer.appendChild(dropdownList);
+
+    return dropdownContainer;
+  }
+
+  static getDateStringFromTimestamp( timestamp ) {
+    // Diff in ms
+    let diff = Date.now() - (timestamp * 1000);
+    if( diff < 1000 ) {
+      return 'just now';
+    }
+    // Diff in seconds
+    diff /= 1000;
+    if( diff < 60 ) {
+      return `${parseInt(diff)} second${parseInt(diff)!==1?'s':''} ago`;
+    }
+    // Diff in minutes
+    diff /= 60;
+    if( diff < 60 ) {
+      return `${parseInt(diff)} minute${parseInt(diff)!==1?'s':''} ago`;
+    }
+    // Diff in hours
+    diff /= 60;
+    if( diff < 24 ) {
+      return `${parseInt(diff)} hour${parseInt(diff)!==1?'s':''} ago`;
+    }
+    // Diff in days
+    diff /= 24;
+    if( diff < 7 ) {
+      return `${parseInt(diff)} day${parseInt(diff)!==1?'s':''} ago`;
+    }
+    // Diff in weeks
+    diff /= 7;
+    if( diff < 4 ) {
+      return `${parseInt(diff)} week${parseInt(diff)!==1?'s':''} ago`;
+    }
+    // Diff in months
+    diff /= 4;
+    if( diff < 13 ) {
+      return `${parseInt(diff)} month${parseInt(diff)!==1?'s':''} ago`;
+    }
+    // Diff in years
+    diff /= 12;
+    if( diff < 7 ) {
+      return `${parseInt(diff)} year${parseInt(diff)!==1?'s':''} ago`;
+    }
+
+    return new Date(timestamp * 1000).toLocaleString();
 
   }
 
